1ea3dde88e182432a0da6aa0e46f20755732c76f
Orsell <34631691+OrsellGaming@users.noreply.github.com>
2023-02-23 22:02:07 -0700

    Updated player inputs,  player controller, and added camera controller
    Updated player inputs,  player controller, and added camera controller

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using Debug = UnityEngine.Debug;
 
+// The main PlayerController for receiving inputs from the user and applying it to the character
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
     private float movementX;
     private float movementY;
 
-    // Start is called before the first frame update
+    // Start is called before the first frame update, or when the game is launched and loaded
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>(); // Assign the player character Rigidbody as a variable to be referenced when 
     }
 
     // Called when a input is received in Unity by a keyboard or controller
@@ -28,6 +29,7 @@ public class PlayerController : MonoBehaviour
         Debug.Log("Moving");
     }
 
+    
     private void FixedUpdate() 
     {
         Vector2 movement = new Vector2(movementX, movementY); // The Vector3 constructor was using the wrong order of arguments
