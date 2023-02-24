414534f04b53700dafb0fd35ff4aa36fecfc5414
Orsell <34631691+OrsellGaming@users.noreply.github.com>
2023-02-23 23:55:02 -0700

    Fixed CameraController and changed Button Selection to Button Template
    Fixed CameraController and changed Button Selection to Button Template

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -4,23 +4,18 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
-    public GameObject player; //Public variable to store a reference to the player game object
-    private Vector3 offset; //Private variable to store the offset distance between the player and camera
+    public GameObject player; // Public variable to store a which player object will be used
+    private Vector3 offset; // Private variable to store the offset distance between the player and camera
 
-    // Start is called before the first frame update, or when the game is launched and loaded
-    void Start () 
+    // Start() is called before the first frame update, or when the game is launched and loaded
+    void Start() 
     {
-        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
+        // Calculate and store the offset value by getting the distance between the player's position and camera's position.
         offset = transform.position - player.transform.position;
     }
 
-    // Update 
-    private void Update() {
-        
-    }
-
     // LateUpdate() is called after Update() each frame
-    void LateUpdate () 
+    private void LateUpdate() 
     {
         // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
         transform.position = player.transform.position + offset;
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,10 +1,11 @@
+using System.IO.Enumeration;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using Debug = UnityEngine.Debug;
 
-// The main PlayerController for receiving inputs from the user and applying it to the character
+// The main PlayerController for receiving inputs from the user and applying it to the player
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
@@ -14,26 +15,25 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update, or when the game is launched and loaded
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>(); // Assign the player character Rigidbody as a variable to be referenced when 
+        // Assign the player character Rigidbody as a variable to be referenced when the Vector 2 attributes of the Rigidbody2D are retrieved
+        rb = GetComponent<Rigidbody2D>();
+        Debug.Log("Player Controller Started");
     }
 
     // Called when a input is received in Unity by a keyboard or controller
     public void OnMove(InputValue movementValue)
     {
-        Vector2 movementVector = movementValue.Get<Vector2>(); // Method name should start with uppercase "G"
-
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        Debug.Log(movementVector);
         movementX = movementVector.x;
         movementY = movementVector.y;
-        Debug.Log(movementX);
-        Debug.Log(movementY);
         Debug.Log("Moving");
     }
 
     
-    private void FixedUpdate() 
+    private void Update() 
     {
         Vector2 movement = new Vector2(movementX, movementY); // The Vector3 constructor was using the wrong order of arguments
         rb.AddForce(movement);
-        Debug.Log(movement);
     }
 }
