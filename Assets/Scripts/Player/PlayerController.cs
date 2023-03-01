8e103c4301a82be9f228f2f3fb702d6491b5d4d7
OrsellGaming <34631691+OrsellGaming@users.noreply.github.com>
2023-03-01 05:07:05 +0000

    Start() for PlayerController done
    Start() for PlayerController done

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,23 +4,28 @@ using UnityEngine.InputSystem;
 // The main PlayerController for receiving inputs from the user and applying it to the player
 public class PlayerController : MonoBehaviour
 {
+    public GameObject StartLocation;
+    public float jumpStrength = 10;
+    public float speed = 8;
+    public int jumpLimit = 2;
+
     private Rigidbody2D rb;
+    private Vector2 startLocationPosition;
     private bool isJumping = false;
     private bool isMoving = false;
     private int jumps = 0;
     private float movementX;
     private float movementY;
 
-    public float jumpStrength = 10;
-    public float speed = 8;
-    public int jumpLimit = 2;
-
     // Start is called before the first frame update, or when the game is launched and loaded
     public void Start()
     {
         // Assign the player character Rigidbody as a variable to be referenced when the Vector 2 attributes of the Rigidbody2D are retrieved
         rb = GetComponent<Rigidbody2D>();
-        rb.
+        startLocationPosition = new Vector2(StartLocation.transform.position.x, StartLocation.transform.position.y);
+        rb.velocity = new Vector2(0.0f, 0.0f);
+        rb.MovePosition(startLocationPosition);
+        rb.Sleep();
     }
 
     // Called when a input is received in Unity by a keyboard or controller
