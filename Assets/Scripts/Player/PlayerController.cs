ec0b03b0ea3531c1e9d9c0670f3ca725ce9eb2a6
OrsellGaming <34631691+OrsellGaming@users.noreply.github.com>
2023-03-02 22:48:17 +0000

    Still trying to get PlayerController working as intended
    Still trying to get PlayerController working as intended

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,43 +7,148 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public GameObject StartLocation;
-    public float jumpStrength = 10;
-    public float speed = 8;
+    public GameObject Ground;
+    public float jumpStrength = 1000;
+    public float speed = 800;
     public int jumpLimit = 2;
 
     private Rigidbody2D rb;
     private Vector2 startLocationPosition;
+    private PlayerInput playerInput;
+    //private PlayerControls playerControls;
     private bool isJumping = false;
     private bool isMoving = false;
+    private bool isGrounded = true;
     private int jumps = 0;
     private float movementX;
     private float movementY;
 
-    // Start is called before the first frame update, or when the game is launched and loaded
+    // Store our control actions as variables to be reused
+    private InputAction moveAction;
+    private InputAction jumpAction;
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// Here the control actions are set and 
+    /// <summary>
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        moveAction = playerInput.actions["Move"];
+        jumpAction = playerInput.actions["Jump"];
+    }
+
+    /// <summary>
+    /// This function is called when the object becomes enabled and active.
+    /// </summary>
+    private void OnEnable()
+    {
+        playerInput.actions.Enable();
+        moveAction.started += context => OnMove(context);
+        jumpAction.started += context => OnJump(context);
+    }
+
+    /// <summary>
+    /// This function is called when the object becomes disabled or inactive.
+    /// </summary>
+    private void OnDisable()
+    {
+        playerInput.actions.Disable();
+        moveAction.started -= context => OnMove(context);
+        jumpAction.started -= context => OnJump(context);
+    }
+    
+    /// <summary>
+    /// Start is called before the first frame update, or when the game is launched and loaded
+    /// <summary>
     public void Start()
     {
         // Assign the player character Rigidbody as a variable to be referenced when the Vector 2 attributes of the Rigidbody2D are retrieved
         rb = GetComponent<Rigidbody2D>();
-        startLocationPosition = new Vector2(StartLocation.transform.position.x, StartLocation.transform.position.y);
+        Vector2 startLocationPosition = new Vector2(StartLocation.transform.position.x, StartLocation.transform.position.y);
         rb.velocity = new Vector2(0.0f, 0.0f);
         rb.MovePosition(startLocationPosition);
         rb.Sleep();
     }
 
-    // Called when a input is received in Unity by a keyboard or controller
-    public void OnMove(InputValue movementValue)
+    /// <summary>
+    /// Called when the player character is touching a GameObject with a Box Collider2d
+    /// </summary>
+    /// <param name="collision"></param>
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        Vector2 movementVector = movementValue.Get<Vector2>();
-        Debug.Log("Moving");
-        movementX = movementVector.x;
-        movementY = movementVector.y;
-        Vector2 movement = new Vector2(movementX, movementY);
-        Debug.Log(movement);
-        rb.AddForce(movement);
+        if (collision.collider.tag == Ground.tag)
+        {
+            isJumping = false;
+            jumps = 0;
+            isGrounded = true;
+        }
     }
 
-    // public void OnMove(InputValue movementValue)
-    // {
-    //     if
-    // }
+    /// <summary>
+    /// Called when the assigned Move control is pressed
+    /// </summary>
+    /// <param name="context"></param>
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Vector2 movementVector = context.ReadValue<Vector2>();
+        movementX = movementVector.x;
+        Debug.Log("OnMove Called");
+        if (context.started)
+        {
+            Debug.Log("MOVING");
+            movementX = movementX * speed;
+            Debug.Log(movementX);
+            Vector2 moveMovement = new Vector2(movementX, 0.0f);
+            rb.AddForce(moveMovement);
+            isMoving = true;
+        }
+        
+        if (context.canceled)
+        {
+            Debug.Log("STOPPED MOVING");
+            movementX = movementX * 0;
+            //Vector2 movement = new Vector2(movementX, 0.0f);
+            if (isGrounded)
+            {   
+                Vector2 vel = rb.velocity;
+                vel.x = 0.0f;
+                rb.velocity = vel;
+                isMoving = false;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Called when the assigned Jump control is pressed
+    /// </summary>
+    /// <param name="movementValue"></param>
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            Debug.Log(jumps);
+            Debug.Log(jumpLimit);
+            jumpStrength = jumpStrength * context.ReadValue<float>();
+            Debug.Log(jumpStrength);
+            Vector2 jumpMovement = new Vector2(0.0f, jumpStrength);
+
+            if (jumps >= jumpLimit)
+            {
+                Debug.Log("Hit jumpLimit");
+                return;
+            }
+
+            rb.AddForce(jumpMovement);
+            jumps += 1;
+            Debug.Log("JUMPING");
+        }
+    }
+
+    /// <summary>
+    /// Update is called every game frame
+    /// </summary>
+    private void Update() {
+        
+    }
 }
